be2c6db60b430d35347b6a2f599613d4cf78cce9
wxing <xingwl0401@gmail.com>
2016-02-25 23:11:49 -0500

    seperate html, ts in app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-        template: '<h1>My colorGame in ng2 begins!</h1>'
+    templateUrl:'app/app.component.html'
         })
 export class AppComponent { }
