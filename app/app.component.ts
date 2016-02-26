01d91f3f64e95f8d6fb1d42af4095d0165cf16f6
wxing <xingwl0401@gmail.com>
2016-02-25 22:59:27 -0500

    add index's css, modify index file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-        template: '<h1>My colorGame in ng2</h1>'
+        template: '<h1>My colorGame in ng2 begins!</h1>'
         })
 export class AppComponent { }
